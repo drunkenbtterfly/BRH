e17bc4cb2a995d0d849f9f1e395f6814687b48aa
irfan <143263391+drunkenbtterfly@users.noreply.github.com>
2024-10-02 22:03:38 +0700

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 p-4">
+    <footer className="bg-black p-4">
       <div className="container mx-auto text-center text-white">
         <p>&copy; 2024 Team BRH</p>
       </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,8 +5,32 @@ import Layout from '../components/Layout';
 const HomePage = () => {
   return (
     <Layout>
-      <div className="container mx-auto p-4">
-        <h1 className="text-2xl font-bold">Test</h1>
+      <div className="bg-black">
+        <div className="container mx-auto p-4">
+          <h1 className="text-2xl font-bold text-white">Masi bingung yagesya</h1>
+        </div>
+
+        <section className='px-20'>
+          <div className='h-40 bg-white rounded-3xl'>
+            
+          </div>
+        </section>
+
+        <section className="py-8">
+          <div className="max-w-4xl mx-auto px-4">
+            <h2 className="text-2xl font-bold mb-4 text-center text-white">Test</h2>
+            <div className="aspect-w-16 aspect-h-9">
+              <iframe
+                className="w-full h-80"
+                src="https://www.youtube.com/embed/yFEcppr--yc"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
+        </section>
       </div>
     </Layout>
   );
